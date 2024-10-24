539d088f63044ea1e28b131571c8bf525abaa407
SamuelRiveros <156517817+SamuelRiveros@users.noreply.github.com>
2024-10-24 00:15:21 -0500

    feat: :sparkles: se refleja la construcción de app.js , la cual se encarga de compilar el back-end como tal

---
+++ server/app.js
@@ -0,0 +1,31 @@
+//dependences
+const express = require('express');
+const cors = require('cors');
+//my files
+const {connection} = require('./infrastructure/db');
+//load envs
+process.loadEnvFile();
+//import routes
+const userRoutes = require('./routes/userRoutes');
+
+//dbConnection
+connection();
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/usuarios', userRoutes);
+
+//start server
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+app.listen({port: PORT, host: HOST}, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
